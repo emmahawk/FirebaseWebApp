84ae2e181bf995774adacf9eb58534855fba29a9
EvanGrote <evanlgrote@gmail.com>
2017-02-19 16:28:04 -0600

    App now writes to firebase database

--- Main.js
+++ Main.js
@@ -10,25 +10,27 @@ var config = {
     authDomain: "iot-lab-2-7359b.firebaseapp.com",
     databaseURL: "https://iot-lab-2-7359b.firebaseio.com",
     storageBucket: "iot-lab-2-7359b.appspot.com",
-    //messagingSenderId: "829587448861"
+    messagingSenderId: "829587448861"
 };
 
-var raspberryPiApp = firebase.initializeApp(config);
+firebase.initializeApp(config);
 var database = firebase.database();
 
-console.log(raspberryPiApp.name);  // "[DEFAULT]"
+console.log(firebase.name);  // "[DEFAULT]"
 
 console.log("Why yes, this is actually working.");
 
 function writeData(humidity, temperature, light_r, light_g, light_b, light_row, light_column, update_light) {
     firebase.database().ref().set({
-        humidity: humidity,
-        temperature: temperature,
-        light_r: light_r,
-        light_g: light_g,
-        light_b: light_b,
-        light_row: light_row,
-        light_column: light_column,
-        update_light: update_light
+        Humidity: humidity,
+        Temperature: temperature,
+        Light_R: light_r,
+        Light_G: light_g,
+        Light_B: light_b,
+        Light_Row: light_row,
+        Light_Column: light_column,
+        Update_Light: update_light
     });
 }
+
+writeData(0, 0, 255, 255, 255, 0, 0, false);
